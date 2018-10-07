d3dc2c308ce35efb9b8b5fdd1b1c54211ce2eea4
susierent <43474188+susiehometest@users.noreply.github.com>
2018-10-07 18:11:50 +0800

    update

--- test_platform/test_platform/urls.py
+++ test_platform/test_platform/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from user_app import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+   path('', views.index),
+    path('login_action/', views.login_action)
 ]
--- test_platform/user_app/views.py
+++ test_platform/user_app/views.py
@@ -1,4 +1,12 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
 # 主要代码逻辑：
+
+def index(request):
+
+    return render(request,"index.html")
+#处理登录请求
+def login_action(request):
+    if request.method=="GET":
+        print(request.GET.get("username"))
