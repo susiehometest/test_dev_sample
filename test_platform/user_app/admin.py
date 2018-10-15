355eefea2e5a0b2b880b125d7d1dec70f39d4924
susierent <43474188+susiehometest@users.noreply.github.com>
2018-10-15 23:49:02 +0800

    update

--- test_platform/user_app/admin.py
+++ test_platform/user_app/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
-
+#把表导进来
+from user_app.models import Project,Module
 # Register your models here.
 # django自带了一个admin后台
+admin.site.register(Project)
+admin.site.register(Module)
